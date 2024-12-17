ca322bb2de53c407aa9577eb06665bec1e920a89
kotto5 <kotto555555@gmail.com>
2024-12-17 20:30:19 +0900

    first commit

---
+++ api/travel-plans.js
@@ -0,0 +1,29 @@
+const axios = require('axios');
+
+const GIST_URL = 'https://gist.githubusercontent.com/kotto5/c1c0490c653ede63937d6e4921d17c50/raw/a64552484935e9c26addc3e998ee7c61eb36c749/travel_plans.json';
+
+module.exports = async (req, res) => {
+  const { path } = req.query;
+  console.log('path', path);
+
+  try {
+    console.log('fetching data');
+    const response = await axios.get(GIST_URL);
+    const data = response.data;
+    console.log('data', data);
+
+    let filteredData = {};
+    return res.status(200).json(data);
+    if (path === '/plan1') {
+      filteredData = data.travel_plans.plan_1;
+    } else if (path === '/plan2') {
+      filteredData = data.travel_plans.plan_2;
+    } else {
+      return res.status(404).json({ error: 'Data not found' });
+    }
+
+    res.status(200).json(filteredData);
+  } catch (error) {
+    res.status(500).json({ error: 'Error fetching data' });
+  }
+};

25551d0a8fcf63f3e5af895f23694b98d93c48d6
kotto5 <kotto555555@gmail.com>
2024-12-17 21:27:16 +0900

    url からdata を探索する

--- api/travel-plans.js
+++ api/travel-plans.js
@@ -1,29 +1,33 @@
-const axios = require('axios');
+const axios = require("axios");
 
-const GIST_URL = 'https://gist.githubusercontent.com/kotto5/c1c0490c653ede63937d6e4921d17c50/raw/a64552484935e9c26addc3e998ee7c61eb36c749/travel_plans.json';
+const GIST_URL =
+  "https://gist.githubusercontent.com/kotto5/c1c0490c653ede63937d6e4921d17c50/raw/4130eab892b0eb44addf1c22298f7ee14379ce92/travel_plans.json";
 
 module.exports = async (req, res) => {
-  const { path } = req.query;
-  console.log('path', path);
+
+  const fullUrl = `https://localhost/${req.url}`;
+  const { searchParams } = new URL(fullUrl);
+  const path = searchParams.get('dest');
+  console.log("searchParams", searchParams);
+  console.log("dest", path);
 
   try {
-    console.log('fetching data');
+    console.log("fetching data");
     const response = await axios.get(GIST_URL);
     const data = response.data;
-    console.log('data', data);
+    console.log("data", data);
 
     let filteredData = {};
-    return res.status(200).json(data);
-    if (path === '/plan1') {
+    if (path === "plan1") {
       filteredData = data.travel_plans.plan_1;
-    } else if (path === '/plan2') {
+    } else if (path === "plan2") {
       filteredData = data.travel_plans.plan_2;
     } else {
-      return res.status(404).json({ error: 'Data not found' });
+      return res.status(404).json({ error: "Data not found" });
     }
 
     res.status(200).json(filteredData);
   } catch (error) {
-    res.status(500).json({ error: 'Error fetching data' });
+    res.status(500).json({ error: "Error fetching data" });
   }
 };
